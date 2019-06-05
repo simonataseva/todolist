706d48728fafb0e04d116aa74afc30adaf60559e
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2019-06-05 10:59:33 +0200

    Adding ul

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello Simona!</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <ul>
+      <li>The capital of Spain is Madrid</li>
+      <li>The capital of Portugal is Lisbon</li>
+      <li>The capital of Macedonia is Skopje</li>
+      <li>The capital of France is Paris</li>
+      <li>The capital of Italy is Rome</li>
+    </ul>, 
+    document.getElementById('root')
+);
+
