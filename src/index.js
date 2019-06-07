a3d3056c4e6b1a9d091ea39284d0e4e7f3cbe846
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2019-06-07 10:22:07 +0200

    Feature - My Info

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <ul>
-      <li>The capital of Spain is Madrid</li>
-      <li>The capital of Portugal is Lisbon</li>
-      <li>The capital of Macedonia is Skopje</li>
-      <li>The capital of France is Paris</li>
-      <li>The capital of Italy is Rome</li>
-    </ul>, 
-    document.getElementById('root')
-);
+function MyInfo() {
+  return (
+    <div>
+      <h1>Simona</h1>
+      <p>I'm from Macedonia, my hometown is called Bitola. I have 24 years, and i love to play football.</p>
+      <ul>
+        <li>Palma de Mallorca</li>
+        <li>Santorini</li>
+        <li>Maldives</li>
+      </ul>
+    </div>
+  )  
+}
 
+ReactDOM.render(
+  <MyInfo />,
+  document.getElementById('root')
+)
