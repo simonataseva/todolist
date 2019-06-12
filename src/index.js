8cba4b269df188ec229f3a914f976334de70cab8
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2019-06-12 12:57:16 +0200

    Creating sample conditions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './style.css'
+
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+  const styles = {
+    fontSize: 30
+  }
+
+  if (hours < 11) {
+    timeOfDay = "morning"
+    styles.color = "#04756F"
+  }
+
+  else if (hours >= 11 && hours < 18) {
+    timeOfDay = "afternoon"
+    styles.color = "#2E0927"
+  }
+
+  else {
+    timeOfDay = "night"
+    styles.color = "#D90000"
+  }
+
+  return (
+    <h1 style={styles}>Good {timeOfDay}!</h1>
+  )
+}
 
 ReactDOM.render(
   <App />,
