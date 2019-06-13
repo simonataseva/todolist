c78f39fd46a0a03ee9928c390b7c9d9e6138e1f6
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2019-06-13 10:47:39 +0200

    Todo list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
-
-import Header from "./components/Header.js"
-import Main from "./components/Main.js"
-import Footer from "./components/Footer.js"
+import TodoItem from "./TodoItem.js"
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />   
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>  
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-  const date = new Date()
-  const hours = date.getHours()
-  let timeOfDay
-  const styles = {
-    fontSize: 30
-  }
-
-  if (hours < 11) {
-    timeOfDay = "morning"
-    styles.color = "#04756F"
-  }
-
-  else if (hours >= 11 && hours < 18) {
-    timeOfDay = "afternoon"
-    styles.color = "#2E0927"
-  }
-
-  else {
-    timeOfDay = "night"
-    styles.color = "#D90000"
-  }
-
-  return (
-    <h1 style={styles}>Good {timeOfDay}!</h1>
-  )
-}
+import App from "./App.js"
+import "./style.css"  
 
 ReactDOM.render(
   <App />,
