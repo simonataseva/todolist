6699c9d89c49082664630e2d95db37df65958478
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2019-06-19 10:55:14 +0200

    Array exercise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react'
+
 import Joke from './Joke'
+import jokesData from "./jokesData.js"
+
 
 function App() {
+  const jokeComponents = jokesData.map(function(joke) {
+    return (
+      <Joke key={joke.id} question={joke.question} punchline={joke.punchLine} />
+    )
+  })
+  
   return (
     <div>
-      <Joke punchLine="It's hard to explain puns to kleptomaniacs because they always take things literally."/>
-      
-      <Joke 
-        question="What's the best thing about Switzerland?" 
-        punchLine="I don't know, but the flag is a big plus."/>
-
-      <Joke 
-        question="Hear about new restaurant called Karma?" 
-        punchLine="There's no menu: You get what you deserve."/>
-
-      <Joke 
-        question="Did you hear about the claustrophobic astronaut?" 
-        punchLine="He just needed a little space."/>    
+      {jokeComponents}   
     </div>
   )
 }
